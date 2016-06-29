9e1f0b3d96d9f2d1a7c962f8a255f50e59c0b5f4
Mox Biggs <moxdev1@gmail.com>
2016-06-29 12:03:37 -0400

    underconstruction

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,12 +1,23 @@
 'use strict';
 
-var generators = require('yeoman-generator');
+var generators = require('yeoman-generator'),
+    mkdirp = require('mkdirp');  //https://www.npmjs.com/package/mkdirpd
 
 module.exports = generators.Base.extend( {
-    method1: function() {
-        console.log('method 1 dog');
-    },
-    method2: function() {
-        console.log('method 2 dog');
+    createProjectFileSystem: function() {
+        var destRoot = this.destinationRoot(),
+            sourceRoot = this.sourceRoot(),
+            appDir = destRoot + '/';
+
+        mkdirp(appDir + '/scripts');
+        mkdirp(appDir + '/test');
+        mkdirp(appDir + '/img');
+
+        // fs.copy takes 2 arguements (source, destination) - http://computercraft.info/wiki/Fs.copy
+        this.fs.copy(sourceRoot + '/.bowerrc', destRoot + '/.bowerrrc')
+        this.fs.copy(sourceRoot + '/bower.json', destRoot + '/bower.json')
     }
 });
+
+
+// will check for conflicts before overwriting files
