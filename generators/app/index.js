faa35fd5959d16655706b651ed92ba409bb7c857
Mox Biggs <moxdev1@gmail.com>
2016-07-05 12:12:28 -0400

    prompt testing

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,47 +1,77 @@
 /*
-*
-* Eaxmples:
-*
-* https://github.com/tutsplus/create-a-custom-yeoman-generator/blob/master/app/index.js
-*
-* https://github.com/yeoman/generator-generator
-*/
+ *
+ * Eaxmples:
+ *
+ * https://github.com/tutsplus/create-a-custom-yeoman-generator/blob/master/app/index.js
+ *
+ * https://github.com/yeoman/generator-generator
+ */
 
 'use strict';
 
 var generators = require('yeoman-generator'),
-    mkdirp = require('mkdirp'),  //https://www.npmjs.com/package/mkdirpd
+    mkdirp = require('mkdirp'), //https://www.npmjs.com/package/mkdirpd
     yosay = require('yosay'),
     chalk = require('chalk');
 
-module.exports = generators.Base.extend( {
+module.exports = generators.Base.extend({
     _createProjectFileSystem: function() {
         var destRoot = this.destinationRoot(),
             sourceRoot = this.sourceRoot(),
-            appDir = destRoot + '/';
+            appDir = destRoot + '/',
+            templateContext = {
+                appname: this.appname,
+                appdescription: this.appdescription,
+                appversion: this.appversion,
+                appauthor: this.appauthor,
+                appemail: this.appemail,
+                applicense: this.applicense
+            };
 
         mkdirp(appDir + '/scripts');
         mkdirp(appDir + '/test');
         mkdirp(appDir + '/img');
 
+        // Yeoman filesystem docs - http://yeoman.io/authoring/file-system.html
         // fs.copy takes 2 arguements (source, destination) - http://computercraft.info/wiki/Fs.copy
-        this.fs.copy(sourceRoot + '/.bowerrc', destRoot + '/.bowerrrc')
-        this.fs.copy(sourceRoot + '/bower.json', destRoot + '/bower.json'),
-        this.fs.copy(sourceRoot + '/package.json', destRoot + '/package.json')
+        // copy.Tpl takes 3 args - https://github.com/sboudrias/mem-fs-editor#copytplfrom-to-context-options
+        this.fs.copy(sourceRoot + '/.bowerrc', destRoot + '/.bowerrc');
+        this.fs.copyTpl(sourceRoot + '/bower.json', destRoot + '/bower.json', templateContext);
+        this.fs.copyTpl(sourceRoot + '/package.json', destRoot + '/package.json', templateContext);
     },
     _getPrompts: function() {
-        var prompts = [
-            {
-                name: 'name',
-                message: 'Name of project?',
-                default: this.appname
-            },{
-                name: 'description',
-                message: 'description'
-            }
-        ];
+        var prompts = [{
+            name: 'name',
+            message: 'What is the name of your project?',
+            default: this.appname
+        }, {
+            name: 'description',
+            message: 'What is a description of your project'
+        }, {
+            name: 'version',
+            message: 'What version is your project? (hint: use semver)',
+            default: '0.0.0'
+        }, {
+            name: 'license',
+            message: 'How is your project licensed?',
+            default: 'MIT'
+        }, {
+            name: 'yourname',
+            message: 'What is your name?',
+        }, {
+            name: 'email',
+            message: 'What is your email?',
+        }];
         return prompts;
-        this.log(prompts);
+    },
+    _saveAnswers: function(answers, callback) {
+        this.appname = answers.name;
+        this.appdescription = answers.description;
+        this.appversion = answers.version;
+        this.applicense = answers.license;
+        this.appauthor = answers.yourname;
+        this.appemail = answers.email;
+        callback();
     },
     initializing: function() {
         var message = chalk.yellow.bold("Welcome to the jungle baby, you gonna die!") + chalk.yellow(" Seriously you're gonna die...");
@@ -50,14 +80,12 @@ module.exports = generators.Base.extend( {
     prompting: function() {
         var done = this.async();
 
-        this.prompt(this._getPrompts(), function(answers){
-            this.appname = answers.name;
-            this.appdescription = answers.description;
-            done();
+        this.prompt(this._getPrompts(), function(answers) {
+            this._saveAnswers(answers, done);
         }.bind(this));
     },
     configuring: function() {
-        this.config.save();
+        this.config.save(); // allows yo to run again in sub directories
     },
     writing: function() {
         this._createProjectFileSystem();
