f812d033a451130712219f5646911e847aa81bd5
Mox Biggs <moxdev1@gmail.com>
2016-07-05 13:18:56 -0400

    testing lodash

--- generators/app/index.js
+++ generators/app/index.js
@@ -18,74 +18,38 @@ module.exports = generators.Base.extend({
     _createProjectFileSystem: function() {
         var destRoot = this.destinationRoot(),
             sourceRoot = this.sourceRoot(),
-            appDir = destRoot + '/',
-            templateContext = {
-                appname: this.appname,
-                appdescription: this.appdescription,
-                appversion: this.appversion,
-                appauthor: this.appauthor,
-                appemail: this.appemail,
-                applicense: this.applicense
-            };
+            appDir = destRoot + '/';
 
         mkdirp(appDir + '/scripts');
         mkdirp(appDir + '/test');
         mkdirp(appDir + '/img');
 
-        // Yeoman filesystem docs - http://yeoman.io/authoring/file-system.html
         // fs.copy takes 2 arguements (source, destination) - http://computercraft.info/wiki/Fs.copy
-        // copy.Tpl takes 3 args - https://github.com/sboudrias/mem-fs-editor#copytplfrom-to-context-options
-        this.fs.copy(sourceRoot + '/.bowerrc', destRoot + '/.bowerrc');
-        this.fs.copyTpl(sourceRoot + '/bower.json', destRoot + '/bower.json', templateContext);
-        this.fs.copyTpl(sourceRoot + '/package.json', destRoot + '/package.json', templateContext);
-    },
-    _getPrompts: function() {
-        var prompts = [{
-            name: 'name',
-            message: 'What is the name of your project?',
-            default: this.appname
-        }, {
-            name: 'description',
-            message: 'What is a description of your project'
-        }, {
-            name: 'version',
-            message: 'What version is your project? (hint: use semver)',
-            default: '0.0.0'
-        }, {
-            name: 'license',
-            message: 'How is your project licensed?',
-            default: 'MIT'
-        }, {
-            name: 'yourname',
-            message: 'What is your name?',
-        }, {
-            name: 'email',
-            message: 'What is your email?',
-        }];
-        return prompts;
-    },
-    _saveAnswers: function(answers, callback) {
-        this.appname = answers.name;
-        this.appdescription = answers.description;
-        this.appversion = answers.version;
-        this.applicense = answers.license;
-        this.appauthor = answers.yourname;
-        this.appemail = answers.email;
-        callback();
+        this.fs.copy(sourceRoot + '/.bowerrc', destRoot + '/.bowerrrc')
+        this.fs.copy(sourceRoot + '/bower.json', destRoot + '/bower.json'),
+            this.fs.copy(sourceRoot + '/package.json', destRoot + '/package.json')
     },
     initializing: function() {
         var message = chalk.yellow.bold("Welcome to the jungle baby, you gonna die!") + chalk.yellow(" Seriously you're gonna die...");
         this.log(yosay(message, { maxLength: 17 }));
     },
     prompting: function() {
-        var done = this.async();
-
-        this.prompt(this._getPrompts(), function(answers) {
-            this._saveAnswers(answers, done);
+        return this.prompt([{
+            type: 'input',
+            name: 'name',
+            message: 'Your project name',
+            default: this.appname // Default to current folder name
+        }, {
+            type: 'input',
+            name: 'description',
+            message: 'Description of generator'
+        }]).then(function(answers) {
+            this.appname = answers.name;
+            this.appdescription = answers.description;
         }.bind(this));
     },
     configuring: function() {
-        this.config.save(); // allows yo to run again in sub directories
+        this.config.save();
     },
     writing: function() {
         this._createProjectFileSystem();
@@ -112,3 +76,16 @@ Run Loop Priorities:
 - install
 - end
  */
+
+    // _getPrompts: function() {
+    //     var prompts = [{
+    //         name: 'name',
+    //         message: 'Name of project?',
+    //         default: this.appname
+    //     }, {
+    //         name: 'description',
+    //         message: 'description'
+    //     }];
+    //     return prompts;
+    //     this.log(prompts);
+    // },
